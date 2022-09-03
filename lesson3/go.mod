6c113926316e63c09075aff89444eefa06aa42b3
NoFateo0 <ne_v_fokuse89@bk.ru>
2022-09-03 20:57:19 +0300

    Homework 3 is done

---
+++ lesson3/go.mod
@@ -0,0 +1,8 @@
+module lesson3
+
+go 1.19
+
+require (
+	github.com/NoFateo0/lesson3_project v0.0.1 // indirect
+	github.com/NoFateo0/lesson3_project/v2 v2.0.0 // indirect
+)
---
+++ lesson3/new_project/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"github.com/NoFateo0/lesson3_project/hello"
+	hello2 "github.com/NoFateo0/lesson3_project/v2/hello"
+)
+
+func main() {
+	hello.SayHello("You")
+	hello2.SayBye("You")
+}
